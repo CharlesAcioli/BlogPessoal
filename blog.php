51a12c4c41e3641bde2761bbcf9f0d12d7ad96e0
Charles Acioli <charlesacioli1@gmail.com>
2025-03-24 17:24:53 -0300

    Blog

---
+++ blog.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="blog.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Blog Pessoal</title>
+    </head>
+    <body>
+        <nav>
+            <a href="mailto:testeemail@email.com"><i class="fa-regular fa-envelope"></i>E-mail</a>
+            <a href="tel:+5582999999999"><i class="fa-solid fa-phone"></i>Ligar</a>
+            <a href="https://wa.me/5582999999999?text=Olá,%20gostaria%20de%20mais%20informações!"><i class="fa-brands fa-whatsapp"></i>WhatsApp</a>
+            <a href="https://github.com/CharlesAcioli/BlogPessoal"><i class="fa-brands fa-github"></i>GitHub</a>
+        </nav>
+
+        <form>
+            <h1>Charles Acioli</h1>
+            <p>Artista nato e amante da técnologia, resolveu focar e embarcar na área tecnológica e tornar como profissão primária, gosta de desafios, soluções matemáticas e estar sempre disposto a aprender. Uma pessoa curiosa, sempre atento as formas de soluções.<br>Buscando atualmente um estágio na área da técnologia. Possui conhecimentos em <strong>HTML, CSS, JavaScript, PHP e banco de dados</strong>. Seu interesse vai além do código, abrangendo também a análise de sistemas, automação e o futuro da tecnologia.<br>Seja desenvolvendo aplicações ou estudando as mais recentes novidades do mercado, está sempre disposto a compartilhar conhecimento e contribuir para o avanço do setor. </p>
+            <img src="https://ita24horas.com.br/blog/wp-content/uploads/2017/05/90226-descubra-a-importancia-do-uso-da-tecnologia-para-seguranca-da-empresa-entregar-em-1705.jpg">
+        </form>
+    </body>
+    <footer>Desenvolvido por <a href="https://www.instagram.com/charles_acioli/#">Charles Acioli</a>&copy;</footer>
+</html>
+
+<?php
